e56e9dc191232eae2bf4d0b5e53c47b7106a7b60
realfoz <scottpool@hotmail.com>
2025-03-27 11:53:46 +0000

    updated leafnode with a a few changes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -23,11 +23,21 @@ class HTMLNode:
 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value=None, props=None):
+    def __init__(self, value, tag=None, props=None):
         super().__init__(tag, value, props)
         
     def to_html(self):
         if not self.value:
-            raise ValueError
+            raise ValueError("must have a value") # removes none value inputs 
+        
+        html = f"<{self.tag}" # adds the initial tag if it has one with the <
+
+        if self.props:
+            for prop, val in self.props.items():    # iterates over each prop to add each tuple to the html string with proper formating
+                html += f' {prop}="{val}"'
+
+        html += f">{self.value}</{self.tag}>" # finalizes the string with final values and tags
+    
+        return html
         
         

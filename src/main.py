4caaf1a64ea57e4c266becd24964464a1411eb05
realfoz <scottpool@hotmail.com>
2025-04-02 16:58:16 +0100

    added file copying to main.py andd updated main function. example file types in static folder

--- src/main.py
+++ src/main.py
@@ -1,8 +1,42 @@
-from node_types import TextNode, TextType
+import os
+import shutil
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    
+    src = "/home/scott/projects/github.com/bootdotdev/StaticSiteGenerator/static"  # Directory containing source files
+    dest = "/home/scott/projects/github.com/bootdotdev/StaticSiteGenerator/public"  # Directory to copy files into
+
+    print(f"Copying files from '{src}' to '{dest}'...")
+
+    # Call file-copying function
+    copy_files_from_static(src, dest)
+
+    print("Static files copied successfully!")
+
+
+
+def copy_files_from_static(src, dest):
+    # Clean and recreate destination directory
+    if os.path.exists(dest):
+        shutil.rmtree(dest)  # Delete destination directory to start fresh
+    os.mkdir(dest)  # Recreate the root directory 
+
+    # Loop through all items in the source directory
+    for item in os.listdir(src):
+        
+        src_path = os.path.join(src, item) 
+        dest_path = os.path.join(dest, item)
+        
+
+        if os.path.isdir(src_path):  # If the item is a directory
+            
+            os.mkdir(dest_path)  
+            # Recursively copy the contents of the directory
+            copy_files_from_static(src_path, dest_path)
+        else:  # If the item is a file
+            
+            shutil.copy(src_path, dest_path)  # Copy the file to the destination
+        
 
 if __name__ =="__main__":
     main()
